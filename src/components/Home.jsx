eff3cebad0d1fe3362262e137f6836e902a6c1b3
Your Name <ronitgandhi96@gmail.com>
2022-08-08 10:18:18 +0530

    added animations.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
 						I'm a{" "}
 						<Typewriter
 							options={{
-								strings: ["a python-developer", "a web-developer."],
+								strings: ["a python-developer.", "a web-developer."],
 								autoStart: true,
 								loop: true,
 							}}
